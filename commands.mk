d091dfb8718aef5fbba01bdc760ee6623190b386
DC23 <jugglindan@gmail.com>
2019-11-13 17:01:08 +1100

    added sample code packaging make target

--- commands.mk
+++ commands.mk
@@ -11,8 +11,11 @@ clean-sample-code :
 	@rm -rf code/nextflow/results/
 	@rm -rf code/nextflow/work/
 	@rm -f code/Pipfile.lock
+	@find code/ -name '*.pyc' -exec rm {} \;
+	@rm -rf code/__pycache__/
 
 ## package-sample-code: package the sample code into downloadable archive
 package-sample-code : clean-sample-code
 	@echo Packing sample code ...
-
+	@cd code && zip -r ../files/workflow-engines-lesson.zip *
+	@cd code && tar czf ../files/workflow-engines-lesson.tar.gz *
