d679f1eb29f6fa16bb18c707f6ae887ea13e2121
DC23 <jugglindan@gmail.com>
2019-11-20 16:12:56 +1100

    explicitly setting AGG backend in plotcount.py so I can remove the matplotlibrc

--- code/plotcount.py
+++ code/plotcount.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import sys
 from collections import Sequence
 
 from wordcount import load_word_counts
+
+matplotlib.use('AGG')
 
 
 def plot_word_counts(counts, limit=10):
@@ -55,18 +58,18 @@ def get_ascii_bars(values, truncate=True, maxlen=10, symbol='#'):
         minimum = min(values) - 1
     else:
         minimum = 0
-    
+
     # Type conversion to floats is required for compatibility with python 2,
     # because it doesn't do integer division correctly (it does floor divison
     # for integers).
     value_range=float(maximum - minimum)
     prop_values = [(float(value - minimum) / value_range) for value in values]
-    
+
     # Type conversion to int required for compatibility with python 2
     biggest_bar = symbol * int(round(maxlen / len(symbol)))
     bars = [biggest_bar[:int(round(prop * len(biggest_bar)))]
             for prop in prop_values]
-    
+
     return bars
 
 
