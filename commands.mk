2b670b89c27c091c7e136bc518a1514c7762f878
DC23 <jugglindan@gmail.com>
2019-12-20 12:58:36 +1100

    updating code sample archives

--- commands.mk
+++ commands.mk
@@ -8,9 +8,10 @@ clean-sample-code :
 		Snakefile config.yaml cluster.yaml results.txt \
 		zipf_analysis.tar.gz Pipfile.lock
 	@find code/ -name '*.pyc' -exec rm {} \;
+	@rm -f ./files/*
 
 ## package-sample-code: package the sample code into downloadable archive
 package-sample-code : clean-sample-code
 	@echo Packing sample code ...
-	@cd code && zip -r ../files/workflow-engines-lesson.zip *
-	@cd code && tar czf ../files/workflow-engines-lesson.tar.gz *
+	@cd code && zip -r ../files/workflow-engines-lesson.zip * .solutions
+	@cd code && tar czf ../files/workflow-engines-lesson.tar.gz * .solutions
