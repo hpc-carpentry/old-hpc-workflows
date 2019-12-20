0b78bd8bc337911a2635ae5fb64a2b6dc1ba68b4
DC23 <jugglindan@gmail.com>
2019-12-20 12:28:56 +1100

    #4 cleaning up for downloadable code packaging

--- commands.mk
+++ commands.mk
@@ -4,15 +4,8 @@
 ## clean-sample-code: clean up outputs from the sample code
 clean-sample-code :
 	@echo Cleaning sample code ...
-	@rm -rf code/snakemake/.snakemake/
-	@rm -rf code/snakemake/results/
-	@rm -rf code/nextflow/.nextflow/
-	@rm -f code/nextflow/.nextflow.log*
-	@rm -rf code/nextflow/results/
-	@rm -rf code/nextflow/work/
-	@rm -f code/Pipfile.lock
+	@rm -rf code/.snakemake/ code/dats/ code/plots/ code/Pipfile.lock code/__pycache__/
 	@find code/ -name '*.pyc' -exec rm {} \;
-	@rm -rf code/__pycache__/
 
 ## package-sample-code: package the sample code into downloadable archive
 package-sample-code : clean-sample-code
