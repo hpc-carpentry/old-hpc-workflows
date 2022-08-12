7994130e382fb65cadeb67d3677ac42fcdee778f
Trevor Keller <trevor.keller@nist.gov>
2022-08-12 19:01:40 -0400

    plot a few theoretical curves

--- code/plot_amdahl.py
+++ code/plot_amdahl.py
@@ -7,54 +7,70 @@ import json
 import matplotlib
 import matplotlib.pyplot as plt
 import sys
+from math import ceil
 matplotlib.use("AGG")  # plot to disk, rather than to a window
 
-
-def amdahl_speedup(parallel_wall_time, serial_wall_time):
+def actual_speedup(parallel_wall_time, serial_wall_time):
     return serial_wall_time / parallel_wall_time
 
 
+def amdahl_speedup(serial_proportion, parallel_proportion, num_cores):
+    normalized_run_time = serial_proportion + (parallel_proportion / num_cores)
+    return 1.0 / normalized_run_time
+
 def analyze_scaling_study(log_files, img_file):
     # Initialize empty variable to hold data(frame)
-    df = None
+    data_frame = None
 
     # Append log files to the dataframe
     for log_file in log_files:
-        with open(log_file, 'r') as f:
+        with open(log_file, "r") as f:
             data = json.loads(f.read())
-            df = pd.concat([df, pd.DataFrame([data])])
+            data_frame = pd.concat([data_frame, pd.DataFrame([data])])
 
     # Sort by processor count, print for reference
-    df.sort_values(by='nproc', inplace=True)
+    data_frame.sort_values(by="nproc", inplace=True)
 
     # Calculate speedup factor
-    serial_wall_time = df.iloc[0]['execution_time']
-    df["speedup"] = df["execution_time"].apply(amdahl_speedup,
-                                               args=(serial_wall_time,))
+    serial_wall_time = data_frame.iloc[0]["execution_time"]
+    data_frame["speedup"] = data_frame["execution_time"].apply(actual_speedup,
+                                                               args=(serial_wall_time,))
 
     # Print out the data frame as a table
-    print(df.to_string(index=False))
+    print(data_frame.to_string(index=False))
 
-    plt.plot(df['nproc'], df["speedup"], 'o', label='Actual')
-
-    # Calculate and plot theoretical speedup factor
-    parallel_proportion = df.iloc[0]['parallel_proportion']
-    serial_proportion = 1 - parallel_proportion
-    processors_range = range(1, df['nproc'].max())
-    speedup = 1.0 / (serial_proportion + (parallel_proportion/processors_range))
-    plt.plot(processors_range, speedup, ':', label='Theoretical')
+    # Create the plot object
+    plt.figure()
 
     # Make graph more legible
-    plt.xlabel('Number of Processes')
-    plt.ylabel('Speedup Factor')
-
-    parallel_percent = int(100 * df.iloc[0]['parallel_proportion'])
-
-    title = "Amdahl's Law Example, {}% parallel".format(parallel_percent)
+    parallel_percent = 100 * data_frame["parallel_proportion"].iloc[0]
+    title = "Amdahl's Law: {:.0f}% Parallel Work".format(parallel_percent)
     plt.title(title)
-    plt.legend(loc="best")
+    plt.xlabel("Number of Processes $N$")
+    plt.ylabel("Speedup Factor $S$")
 
-    plt.savefig(img_file, dpi=400, bbox_inches='tight')
+    # Print the scaling study data
+    plt.plot(data_frame["nproc"], data_frame["speedup"], "o", label="actual data")
+
+    # Save the limits of the plotted data in x and y
+    data_range = [plt.xlim(), plt.ylim()]
+
+    # Calculate and plot theoretical speedup factor
+    markers = matplotlib.lines.Line2D.filled_markers
+    for index, parallel_proportion in enumerate((0.7, 0.8, 0.9, 1)):
+        serial_proportion = 1 - parallel_proportion
+        num_cores = range(1, data_frame["nproc"].max() + 1)
+        speedup = [amdahl_speedup(serial_proportion, parallel_proportion, x) for x in num_cores]
+        label = r"theory, $p=%.2f$" % parallel_proportion
+        plt.plot(num_cores, speedup, linestyle=":", marker=markers[index+1], markersize=4, label=label, zorder=0)
+
+    # Reset plot limits to the actual data
+    plt.xlim(data_range[0])
+    plt.ylim(data_range[1])
+
+    # Show the legend and store the plot as an image
+    plt.legend(loc="best")
+    plt.savefig(img_file, dpi=400, bbox_inches="tight")
 
 
 if __name__ == "__main__":
