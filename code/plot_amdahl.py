794f8a48c6168fe5c65780f1ec41c71a6b681b15
Trevor Keller <trevor.keller@nist.gov>
2022-08-12 17:43:41 -0400

    capture Slurm output, use 'if __name__ == main' pattern

--- code/plot_amdahl.py
+++ code/plot_amdahl.py
@@ -9,47 +9,66 @@ import matplotlib.pyplot as plt
 import sys
 matplotlib.use("AGG")  # plot to disk, rather than to a window
 
-usage = "plot_amdahl.py list_of_inputs.json"
 
-# Check whether filenames were supplied
-if len(sys.argv) == 0:
-    print(usage)
-    sys.exit(1)
+def amdahl_speedup(parallel_wall_time, serial_wall_time):
+    return serial_wall_time / parallel_wall_time
 
-# Get list of log files
-log_files = sys.argv[1:]
 
-df = None  # Initialize an empty DataFrame
+def analyze_scaling_study(log_files, img_file):
+    # Initialize empty variable to hold data(frame)
+    df = None
 
-# Append log files to the dataframe
-for log_file in log_files:
-    with open(log_file, 'r') as f:
-        data = json.loads(f.read())
-        df = pd.concat([df, pd.DataFrame([data])])
+    # Append log files to the dataframe
+    for log_file in log_files:
+        with open(log_file, 'r') as f:
+            data = json.loads(f.read())
+            df = pd.concat([df, pd.DataFrame([data])])
 
-# Sort by processor count, print for reference
-df.sort_values(by='nproc', inplace=True)
-print(df)
+    # Sort by processor count, print for reference
+    df.sort_values(by='nproc', inplace=True)
 
-# Calculate and plot speedup factor
-serial_time = df.iloc[0]['execution_time']
-plt.plot(df['nproc'], serial_time/df['execution_time'], 'o', label='Actual')
+    # Calculate speedup factor
+    serial_wall_time = df.iloc[0]['execution_time']
+    df["speedup"] = df["execution_time"].apply(amdahl_speedup,
+                                               args=(serial_wall_time,))
 
-# Calculate and plot theoretical speedup factor
-parallel_proportion = df.iloc[0]['parallel_proportion']
-serial_proportion = 1 - parallel_proportion
-processors_range = range(1, df['nproc'].max())
-speedup = 1.0 / (serial_proportion + (parallel_proportion/processors_range))
-plt.plot(processors_range, speedup, ':', label='Theoretical')
+    # Print out the data frame as a table
+    print(df.to_string(index=False))
 
-# Make graph more legible
-plt.xlabel('Number of Processes')
-plt.ylabel('Speedup Factor')
-plt.legend()
-parallel_percent = int(100 * df.iloc[0]['parallel_proportion'])
+    plt.plot(df['nproc'], df["speedup"], 'o', label='Actual')
 
-title = "Amdahl's Law Example, {}% parallel".format(parallel_percent)
-plt.title(title)
+    # Calculate and plot theoretical speedup factor
+    parallel_proportion = df.iloc[0]['parallel_proportion']
+    serial_proportion = 1 - parallel_proportion
+    processors_range = range(1, df['nproc'].max())
+    speedup = 1.0 / (serial_proportion + (parallel_proportion/processors_range))
+    plt.plot(processors_range, speedup, ':', label='Theoretical')
 
-filename = 'amdahl-{}-pct-parallel.png'.format(parallel_percent)
-plt.savefig(filename, dpi=400, bbox_inches='tight')
+    # Make graph more legible
+    plt.xlabel('Number of Processes')
+    plt.ylabel('Speedup Factor')
+
+    parallel_percent = int(100 * df.iloc[0]['parallel_proportion'])
+
+    title = "Amdahl's Law Example, {}% parallel".format(parallel_percent)
+    plt.title(title)
+    plt.legend(loc="best")
+
+    plt.savefig(img_file, dpi=400, bbox_inches='tight')
+
+
+if __name__ == "__main__":
+    # Check whether filenames were supplied
+    usage = "plot_amdahl.py {input_json_files} output_png_file"
+
+    if len(sys.argv) == 0 or (".png" not in sys.argv[-1]):
+        print(usage)
+        sys.exit(1)
+
+    # Get list of log file names
+    log_files = sys.argv[1:-1]
+
+    # Set plot image file name
+    img_file = sys.argv[-1]
+
+    analyze_scaling_study(log_files, img_file)
