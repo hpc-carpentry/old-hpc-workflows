78b4ae39a069fb94e8d9714edaf47e70f113a278
Trevor Keller <trevor.keller@nist.gov>
2022-08-12 14:07:37 -0400

    feed list of inputs to plotting script (more Snakemake-thonic)

--- code/plot_amdahl.py
+++ code/plot_amdahl.py
@@ -3,21 +3,29 @@ Plotting script for HPC Workflows scaling study
 """
 
 import pandas as pd
-import glob
 import json
 import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use("AGG")
+import sys
+matplotlib.use("AGG")  # plot to disk, rather than to a window
 
-# Get list of log files, read first one into dataframe
-log_files = glob.glob('amdahl_*.json')
-with open(log_files[0], 'r') as f:
-    df = pd.DataFrame([json.loads(f.read())])
+usage = "plot_amdahl.py list_of_inputs.json"
 
-# Append rest of log files into same dataframe
-for log_file in log_files[1:]:
+# Check whether filenames were supplied
+if len(sys.argv) == 0:
+    print(usage)
+    sys.exit(1)
+
+# Get list of log files
+log_files = sys.argv[1:]
+
+df = None  # Initialize an empty DataFrame
+
+# Append log files to the dataframe
+for log_file in log_files:
     with open(log_file, 'r') as f:
-        df = pd.concat([df, pd.DataFrame([json.loads(f.read())])])
+        data = json.loads(f.read())
+        df = pd.concat([df, pd.DataFrame([data])])
 
 # Sort by processor count, print for reference
 df.sort_values(by='nproc', inplace=True)
@@ -29,8 +37,9 @@ plt.plot(df['nproc'], serial_time/df['execution_time'], 'o', label='Actual')
 
 # Calculate and plot theoretical speedup factor
 parallel_proportion = df.iloc[0]['parallel_proportion']
+serial_proportion = 1 - parallel_proportion
 processors_range = range(1, df['nproc'].max())
-speedup = 1.0/((1-parallel_proportion)+parallel_proportion/processors_range)
+speedup = 1.0 / (serial_proportion + (parallel_proportion/processors_range))
 plt.plot(processors_range, speedup, ':', label='Theoretical')
 
 # Make graph more legible
@@ -42,5 +51,5 @@ parallel_percent = int(100 * df.iloc[0]['parallel_proportion'])
 title = "Amdahl's Law Example, {}% parallel".format(parallel_percent)
 plt.title(title)
 
-filename = 'amdahl-{}-percent.png'.format(parallel_percent)
+filename = 'amdahl-{}-pct-parallel.png'.format(parallel_percent)
 plt.savefig(filename, dpi=400, bbox_inches='tight')
